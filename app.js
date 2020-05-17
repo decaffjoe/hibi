ce30e65e2defadc2ef345faffe48cdcb85de771e
decaffjoe <jw3mail+decaff@pm.me>
2020-05-17 03:35:29 -0500

    Daily character MVP

--- app.js
+++ app.js
@@ -1,58 +1,149 @@
 "use strict";
-const tooltip = "UwwwuuUu it's " + new Date().toLocaleTimeString(), char = {}, url = "https://graphql.anilist.co", body = document.body, titleAnime = document.querySelector('h1#anime'), inputSearch = document.querySelector('input'), showResults = document.querySelector('#show-results');
-titleAnime.title = tooltip;
-inputSearch?.addEventListener('keyup', getShowNames);
-async function getShowNames(e) {
-    let search = e.target.value, shows = [];
-    if (search.length > 0) {
-        await fetch(url, {
+const tooltip = "UwwwuuUu it's " + new Date().toLocaleTimeString(), char = {}, url = "https://graphql.anilist.co", body = document.body, charImage = document.querySelector('#charDay div'), charH1 = document.querySelector('#charDay h1'), charH2 = document.querySelector('#charDay h2'), charP = document.querySelector('#charDay p'), charTitles = document.querySelector('#charDay ul'), inputSearch = document.querySelector('input'), showResults = document.querySelector('#show-results');
+// Tooltip for character of the day header
+charH1.title = tooltip;
+// DAILY CHARACTER
+main();
+async function main() {
+    try {
+        const popShows = await getPopularShows();
+        const targetShowId = await pickRandomId(popShows);
+        const targetShow = await getShowCharacters(targetShowId);
+        const targetShowNames = [];
+        for (let key in targetShow.title) {
+            targetShowNames.push(targetShow.title[key]);
+        }
+        let childLen = charTitles.children.length;
+        for (let i = 0; i < childLen; ++i) {
+            charTitles.children[i].textContent = targetShowNames[i];
+        }
+        const targetShowChars = targetShow.characters.nodes;
+        const targetCharId = await pickRandomId(targetShowChars);
+        const dailyChar = await getDailyCharacter(targetCharId);
+        charImage.style.backgroundImage = `url("${dailyChar.image.large}")`;
+        charH2.textContent = dailyChar.name.full;
+        charP.innerHTML = dailyChar.description;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+// Get daily character
+async function getDailyCharacter(id) {
+    try {
+        let res = await fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type": 'application/json',
                 'Accept': 'application/json'
             },
             body: JSON.stringify({
+                // match ShowData type
                 query: `
-                query getShowNames { 
-                    Page (page: 1, perPage: 6) {
-                        media (search: "${search}") {
-                            id 
-                            title {
-                                romaji 
-                            } 
-                        } 
+                query getDailyCharacter {
+                    Character(id: ${id}) {
+                      name {
+                        first
+                        last
+                        full
+                        native
+                      }
+                      description
+                      image {
+                        large
+                        medium
+                      }
                     }
                 }
                 `,
             })
-        })
-            .then(res => res.json())
-            .then(data => data.data.Page.media)
-            .then(data => shows = data)
-            .catch(err => console.log(err));
-    }
-    else {
-        shows = [];
-    }
-    await showShowNames(shows);
-    colorFader(e, 'background-color');
-}
-;
-async function showShowNames(shows) {
-    while (showResults?.firstChild) {
-        showResults.removeChild(showResults.lastChild);
-    }
-    ;
-    if (shows.length > 0) {
-        await shows.forEach(show => {
-            let el = document.createElement('li');
-            el.textContent = show.title.romaji;
-            showResults?.appendChild(el);
         });
+        res = await res.json();
+        res = res.data.Character;
+        return res;
+    }
+    catch (err) {
+        console.log(err);
+        return err;
     }
 }
-;
-function colorFader(e, field) {
+// Get target show information
+async function getShowCharacters(id) {
+    try {
+        let res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                // match ShowData type
+                query: `
+                query getShowCharacters {
+                    Media(id: ${id}) {
+                      title {
+                        romaji
+                        english
+                        native
+                      }
+                      characters(sort: FAVOURITES_DESC) {
+                        nodes {
+                          id
+                        }
+                      }
+                    }
+                  }
+                `,
+            })
+        });
+        res = await res.json();
+        res = res.data.Media;
+        return res;
+    }
+    catch (err) {
+        console.log(err);
+        return err;
+    }
+}
+// Pick random value from array of objects w/ id
+function pickRandomId(arr) {
+    const len = arr.length;
+    const index = Math.floor(Math.random() * len);
+    return arr[index].id;
+}
+// Get most popular shows
+async function getPopularShows() {
+    try {
+        let res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                // match ShowData type
+                query: `
+        query getPopularShows {
+            first50: Page(page: 1, perPage: 50) {
+              media(sort: POPULARITY_DESC) {
+                id
+              }
+            }
+          }
+        `,
+            })
+        });
+        res = await res.json();
+        res = res.data.first50.media;
+        return res;
+    }
+    catch (err) {
+        console.log(err);
+        return [];
+    }
+}
+// Create random rgb color and apply to property 'field' of event target's style
+function randRGBEventStyler(e, field) {
     let rand = [1, 1, 1];
     rand = rand.map(x => Math.random() * 255);
     let bgRand = `rgb(${rand[0]}, ${rand[1]}, ${rand[2]})`;
--- app.ts
+++ app.ts
@@ -2,66 +2,199 @@ const tooltip = "UwwwuuUu it's " + new Date().toLocaleTimeString(),
     char = {},
     url = "https://graphql.anilist.co",
     body = document.body,
-    titleAnime = document.querySelector('h1#anime'),
+    charImage = document.querySelector('#charDay div'),
+    charH1 = document.querySelector('#charDay h1'),
+    charH2 = document.querySelector('#charDay h2'),
+    charP = document.querySelector('#charDay p'),
+    charTitles = document.querySelector('#charDay ul'),
     inputSearch = document.querySelector('input'),
     showResults = document.querySelector('#show-results');
 
-titleAnime.title = tooltip;
-inputSearch?.addEventListener('keyup', getShowNames);
+// Tooltip for character of the day header
+charH1.title = tooltip;
 
-async function getShowNames(e: Event) {
-    let search: String = e.target.value,
-        shows: any[] = [];
+// DAILY CHARACTER
+main();
 
-    if (search.length > 0) {
-        await fetch(url, {
+async function main() {
+    try {
+        const popShows = await getPopularShows();
+        const targetShowId = await pickRandomId(popShows);
+
+        const targetShow = await getShowCharacters(targetShowId);
+
+        const targetShowNames: string[] = [];
+        for (let key in targetShow.title) {
+            targetShowNames.push(targetShow.title[key]);
+        }
+        let childLen = charTitles.children.length;
+        for (let i = 0; i < childLen; ++i) {
+            charTitles.children[i].textContent = targetShowNames[i];
+        }
+
+        const targetShowChars = targetShow.characters.nodes;
+        const targetCharId = await pickRandomId(targetShowChars);
+
+        const dailyChar = await getDailyCharacter(targetCharId);
+        charImage.style.backgroundImage = `url("${dailyChar.image.large}")`;
+        charH2.textContent = dailyChar.name.full;
+        charP.innerHTML = dailyChar.description;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Get daily character
+async function getDailyCharacter(id: number): Promise<dailyCharacter> {
+    try {
+        let res = await fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type": 'application/json',
                 'Accept': 'application/json'
             },
             body: JSON.stringify({
+                // match ShowData type
                 query: `
-                query getShowNames { 
-                    Page (page: 1, perPage: 6) {
-                        media (search: "${search}") {
-                            id 
-                            title {
-                                romaji 
-                            } 
-                        } 
+                query getDailyCharacter {
+                    Character(id: ${id}) {
+                      name {
+                        first
+                        last
+                        full
+                        native
+                      }
+                      description
+                      image {
+                        large
+                        medium
+                      }
                     }
                 }
                 `,
             })
-        })
-            .then(res => res.json())
-            .then(data => data.data.Page.media)
-            .then(data => shows = data)
-            .catch(err => console.log(err));
-    } else {
-        shows = [];
-    }
-    await showShowNames(shows);
-    colorFader(e, 'background-color');
-};
-
-async function showShowNames(shows: any[]) {
-    while (showResults?.firstChild) {
-        showResults.removeChild(showResults.lastChild)
-    };
-    if (shows.length > 0) {
-        await shows.forEach(show => {
-            let el = document.createElement('li');
-            el.textContent = show.title.romaji;
-            showResults?.appendChild(el);
         });
+        res = await res.json();
+        res = res.data.Character;
+        return res;
+    } catch (err) {
+        console.log(err);
+        return err;
     }
-};
+}
 
-function colorFader(e, field: String) {
-    let rand: Number[] = [1, 1, 1];
+interface dailyCharacter {
+    name: {
+        first: string,
+        last: string,
+        full: string,
+        native: string
+    },
+    description: string,
+    image: {
+        large: string,
+        medium: string
+    }
+}
+
+// Get target show information
+async function getShowCharacters(id: number): Promise<showCharacters> {
+    try {
+        let res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                // match ShowData type
+                query: `
+                query getShowCharacters {
+                    Media(id: ${id}) {
+                      title {
+                        romaji
+                        english
+                        native
+                      }
+                      characters(sort: FAVOURITES_DESC) {
+                        nodes {
+                          id
+                        }
+                      }
+                    }
+                  }
+                `,
+            })
+        });
+        res = await res.json();
+        res = res.data.Media;
+        return res;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
+
+// Pick random value from array of objects w/ id
+function pickRandomId(arr: []): number {
+    const len = arr.length;
+    const index = Math.floor(Math.random() * len);
+    return arr[index].id;
+}
+
+// Get most popular shows
+async function getPopularShows(): Promise<popularShows[]> {
+    try {
+        let res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                // match ShowData type
+                query: `
+        query getPopularShows {
+            first50: Page(page: 1, perPage: 50) {
+              media(sort: POPULARITY_DESC) {
+                id
+              }
+            }
+          }
+        `,
+            })
+        });
+        res = await res.json();
+        res = res.data.first50.media;
+        return res;
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+}
+
+// match getShowNames fetch query
+interface popularShows {
+    id: number
+}
+
+interface showCharacters {
+    title: {
+        romaji: string,
+        english: string,
+        native: string
+    },
+    characters: {
+        nodes: {
+            [key: string]: number
+        }
+    }
+}
+
+// Create random rgb color and apply to property 'field' of event target's style
+function randRGBEventStyler(e: Event, field: string): void {
+    let rand = [1, 1, 1];
     rand = rand.map(x => Math.random() * 255);
-    let bgRand: String = `rgb(${rand[0]}, ${rand[1]}, ${rand[2]})`;
+    let bgRand = `rgb(${rand[0]}, ${rand[1]}, ${rand[2]})`;
     e.target.style = `${field}: ${bgRand};`;
 };
