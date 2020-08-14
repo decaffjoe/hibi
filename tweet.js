4ba0237915a29e59f18525390c2f551d0494f120
decaffjoe <jw3mail+decaff@pm.me>
2020-08-13 23:18:00 -1000

    New favicon. WIP twitter bot (aws lambda auth!)

---
+++ tweet.js
@@ -0,0 +1,25 @@
+"use strict";
+const Twitter = require('twitter-lite'), nodeFetch = require('node-fetch');
+async function main() {
+    // get credentials from aws lambda
+    const res = await nodeFetch('https://hotwu4uar1.execute-api.us-east-2.amazonaws.com/default/hibi-twitter-bot-auth');
+    if (res.status === 200) {
+        const { consumer_key, consumer_secret, access_token_key, access_token_secret } = await res.json();
+        const client = new Twitter({
+            consumer_key,
+            consumer_secret,
+            access_token_key,
+            access_token_secret
+        });
+        // retrieve tweet data from data.json
+        // post the tweet
+        // await client.post("statuses/update", {
+        //     status,
+        //     media_ids
+        // });
+    }
+    else {
+        console.log(`aws failure: received code ${res.status}`);
+    }
+}
+main().catch(err => console.log(err));
