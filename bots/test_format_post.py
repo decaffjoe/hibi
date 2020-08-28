415de7358308c5b8ce483462e456f651058594a5
decaffjoe <jw3mail+decaff@pm.me>
2020-08-27 20:56:35 -0500

    Add tweet/toot unit testing

--- bots/test_format_post.py
+++ bots/test_format_post.py
@@ -1,25 +1,33 @@
 import unittest
 import format_post
 
-class TestFormatPost(unittest.TestCase):
 
+class TestFormatPost(unittest.TestCase):
     def test_shorten_status(self):
         status = 'something that is more than 50 characters something that is more than 50 characters'
-        closing = '...bye!' # matches [-7:]
+        closing = '...bye!'  # matches [-7:]
         limit = 50
 
         # Status is <= limit
-        self.assertLessEqual(50, len(format_post.shorten_status(status, closing, limit)))
+        self.assertLessEqual(
+            50, len(format_post.shorten_status(status, closing, limit)))
         # Closing is attached (if over limit)
-        self.assertEqual(closing, format_post.shorten_status(status, closing, limit)[-7:])
+        self.assertEqual(
+            closing,
+            format_post.shorten_status(status, closing, limit)[-7:])
         # Closing is NOT attached (if under limit)
-        self.assertNotEqual(closing, format_post.shorten_status(status[:50], closing, limit)[-7:])
+        self.assertNotEqual(
+            closing,
+            format_post.shorten_status(status[:50], closing, limit)[-7:])
 
     def test_clean_description(self):
         # Remove HTML tags/codes & spoilers
-        self.assertTrue('</p>' not in format_post.clean_description('<p>a tag</p>'))
-        self.assertTrue('&quot;' not in format_post.clean_description('he then &quot; went swimming'))
-        self.assertTrue('!~' not in format_post.clean_description('~! spoiler!! !~'))
+        self.assertTrue(
+            '</p>' not in format_post.clean_description('<p>a tag</p>'))
+        self.assertTrue('&quot;' not in format_post.clean_description(
+            'he then &quot; went swimming'))
+        self.assertTrue(
+            '!~' not in format_post.clean_description('~! spoiler!! !~'))
 
     def test_image_mime(self):
         mimes = {
@@ -38,10 +46,16 @@ class TestFormatPost(unittest.TestCase):
                 self.assertTrue(format_post.image_mime(ext) == mime)
 
         # Incorrect img extensions are NOT supported
+        # self.assertRaises(ValueError, format_post.image_mime, 'jpg')
         with self.assertRaises(ValueError):
             format_post.image_mime('jpg')
+
+        with self.assertRaises(ValueError):
             format_post.image_mime('.jpgg')
+
+        with self.assertRaises(ValueError):
             format_post.image_mime('.mp4')
+
 
 if __name__ == '__main__':
     unittest.main()
